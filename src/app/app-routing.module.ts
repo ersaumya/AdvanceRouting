160e8ac62892758b7eb6d67db3c092fcbaceabe2
ersaumya <ersaumyarout@gmail.com>
2020-03-03 18:05:38 +0530

    custom preload strategy added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { DashboardModule } from './dashboard/dashboard.module';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, PreloadingStrategy, Route, preload } from '@angular/router';
+import { Observable,of } from 'rxjs';
 
+/* custom preload */
+export class CustomPreload implements PreloadingStrategy{
+  preload(route:Route,fn:()=>Observable<any>):Observable<any>{
+    return route.data && route.data.preload ? fn(): of(null);
+  }
+}
 
 export const mainRoutes: Routes = [
-  { path:'dashboard',
+  { path:'dashboard',data:{preload:true},
   loadChildren:()=>import('./dashboard/dashboard.module').then(m=>m.DashboardModule)},
   { path: '**', redirectTo: 'mail/folder/inbox' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(mainRoutes /*, {enableTracing:true} */,{preloadingStrategy:PreloadAllModules})],
+  imports: [RouterModule.forRoot(mainRoutes, { preloadingStrategy: CustomPreload})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { MailModule } from './mail/mail.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, CustomPreload } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [CustomPreload],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
